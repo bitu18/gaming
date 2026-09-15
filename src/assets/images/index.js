fd29e7b1ea5009929f40b3f1ec6265ae4cf23983
Bitu <bitu@Bitu.>
2026-09-15 18:28:46 -0500

    Update home page images and styling

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,8 +3,8 @@ const images = {
     // logoGaming: require('~/assets/images/logo-try.png'),
     defaultImage: require('~/assets/images/default-image.jpg'),
     // bannerHome: require('~/assets/images/banner-custom.jpg'),
-    bannerHome: require('~/assets/images/banner.png'),
     // bannerHome: require('~/assets/images/banner.png'),
+    bannerHome: require('~/assets/images/banner-try.png'),
 };
 
 export default images;
